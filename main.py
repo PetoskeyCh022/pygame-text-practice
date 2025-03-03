07d5f1e1f6c97489370e85be21f54ab2b7a5bfd0
PetoskeyCh022 <PetoskeyCh022@northwested.org>
2025-03-03 10:50:00 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -17,20 +17,54 @@ def handle_events():
         return False
   return True
 
+
+def draw_text(screen, text, font, color, x_cord, y_cord):
+  img = font.render(text, True, color)
+  screen.blit(img, (x_cord, y_cord))
+
+
+
+
+
 def main():
   screen = init_game()
   clock = pygame.time.Clock() # Initialize the clock here
   running = True
+
+  text_font1 = pygame.font.SysFont(None, 30, False, True)
+
+  text_font2 = pygame.font.SysFont('Times New Roman', 50, True, False)
+
+  text_font3 = pygame.font.Font("FreeMono.ttf", 30)
+
+
+
   while running:
     running = handle_events()
     screen.fill(config.WHITE) # Use color from config
+
+
+    draw_text(screen, "Charles Petoskey", text_font1, config.GREEN, 300, 200)
+
+    draw_text(screen, "Career Tech Center", text_font2, config.RED, 200, 300)
+
+    draw_text(screen, "Career Tech Center", text_font3, config.RED, 200, 500)
+
+
+
     pygame.display.flip()
 
     # Limit the frame rate to the specified frames per second (FPS)
     clock.tick(config.FPS) # Use the clock to control the frame rate
 
+  
   pygame.quit()
   sys.exit()
+
+
+
+
+
 
 if __name__ == "__main__":
   main()
